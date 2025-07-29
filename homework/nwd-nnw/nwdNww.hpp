206d004b856f0f93072ebf94768edab9563d6ed5
pstyczynski <patrykstyczynski55@gmail.com>
2025-07-29 23:00:44 +0200

    adding works

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,58 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int max=0;
+
+    for(int i=1; i<100; i++)
+    {
+        if(lhs==0)
+        {
+            return rhs;
+        }
+        else if(rhs==0)
+        {
+            return lhs;
+        }
+        if(lhs%i==0 && rhs%i==0)
+        {
+            max=i;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return max;
 }
 
+
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+
+    unsigned long max=0;
+    unsigned long i=0;
+    for(i=1; i<5000000; i++)
+    {
+        if(lhs == 0 || rhs == 0)
+        {
+            return 0;
+        }
+        // if(lhs<0)
+        // {
+        //     return -rhs;
+        // }
+        // else if(rhs<0)
+        // {
+        //     return -lhs;
+        // }
+        if(i%abs(lhs)==0 && i%abs(rhs)==0)
+        {
+            max=i;
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+    return max;
 }
