08027113e503a20338b073022385dbfc71783e7b
pstyczynski <patrykstyczynski55@gmail.com>
2025-07-29 23:17:53 +0200

    adding works

--- homework/max-of-vector/maxOfVector.hpp
+++ homework/max-of-vector/maxOfVector.hpp
@@ -3,6 +3,16 @@
 #include <vector>
 
 int maxOfVector(const std::vector<int>& vec) {
-    // TODO: Implement me :)
-    return {};
+    long results=-3000000;
+    long temp=0;
+    for(size_t i=0; i<vec.size(); i++)
+    {
+        temp=vec[i];
+        if(temp>results)
+        {
+            results=temp;
+        }
+    }
+
+    return static_cast<int>(results);
 }
